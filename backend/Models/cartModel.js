83f7afdcea9115a80221d523d4953067ee600a5c
ashikulislamcse <ashikulislamcse@gmail.com>
2025-04-10 23:40:37 +0600

    Cart Model

---
+++ backend/Models/cartModel.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const cartSchema = new mongoose.Schema(
+  {
+    productId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Product",
+    },
+    quantity: {
+      type: Number,
+      default: 1,
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+const CartModel = mongoose.model("Cart", cartSchema);
+export default CartModel;
