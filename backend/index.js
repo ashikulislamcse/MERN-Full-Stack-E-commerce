56802a98af3386dcac85d2196783cd4730b99fc3
ashikulislamcse <ashikulislamcse@gmail.com>
2025-04-10 20:35:09 +0600

    Updated

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+
+const app  = express();
+app.use(cors(
+    credentials = true,
+    origin = process.env.FRONTEND_URL
+));
+
+
+
+
+
