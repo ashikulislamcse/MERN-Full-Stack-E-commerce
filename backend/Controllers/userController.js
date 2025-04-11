21d865fd3a04856270fb588dcf24d45a1496a218
ashikulislamcse <ashikulislamcse@gmail.com>
2025-04-11 12:06:52 +0600

    Login Working

--- backend/Controllers/userController.js
+++ backend/Controllers/userController.js
@@ -5,6 +5,8 @@ import sendMail from "../config/sendEmail.js";
 import verifyEmailTemplate from "../Utils/VerifyEmailTemplete.js";
 dotenv.config();
 
+// User Registration
+
 export const registerUser = async (req, res) => {
   try {
     const { name, email, password } = req.body;
@@ -43,6 +45,65 @@ export const registerUser = async (req, res) => {
       message: "User registered successfully",
       data: newUser,
     });
+  } catch (error) {
+    return res.status(500).json({ success: false, message: error.message });
+  }
+};
+
+// Email Verification
+export const verifyEmail = async (req, res) => {
+  try {
+    const { code } = req.body;
+    const user = await User.findOne({ _id: code });
+
+    if (!user) {
+      return res
+        .status(404)
+        .json({ success: false, message: "User not found" });
+    }
+    if (user.verify_email) {
+      return res
+        .status(400)
+        .json({ success: false, message: "User already verified" });
+    }
+    user.verify_email = true;
+    await user.save();
+    return res
+      .status(200)
+      .json({ success: true, message: "User verified successfully" });
+  } catch (error) {
+    return res.status(500).json({ success: false, message: error.message });
+  }
+};
+
+// User Login
+
+export const userLogin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ success: false, message: "All fields are required" });
+    }
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res
+        .status(400)
+        .json({ success: false, message: "User Not Found!" });
+    }
+    if (user.status !== "Active") {
+      return res
+        .status(400)
+        .json({ success: false, message: "Account not Active" });
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Invalid Password" });
+    }
+    
   } catch (error) {
     return res.status(500).json({ success: false, message: error.message });
   }
--- backend/Routes/userRoute.js
+++ backend/Routes/userRoute.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { registerUser } from '../Controllers/userController.js';
+import { registerUser, verifyEmail } from '../Controllers/userController.js';
 
 
 
 const userRouter = express.Router();
 
 userRouter.post('/register', registerUser);
+userRouter.post('/verify-email', verifyEmail);
 
 
 export default userRouter;
